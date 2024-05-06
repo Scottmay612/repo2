ac213ec4c6f8c73b460335165315150c92c95b15
Scott May <scottmay612@gmail.com>
2024-05-06 12:24:50 -0600

    I did it.

--- shapes.py
+++ shapes.py
@@ -1,7 +1,12 @@
 # Python
 
 # This will calculate the shapes.
+def calculateAreaRectangle(width, length):
+    area = width * length
+    return area
+
 def main():
     print("This is our shapes program!")
+    print(f"The area is {calculateAreaRectangle(10,20)}")
 
 main()
